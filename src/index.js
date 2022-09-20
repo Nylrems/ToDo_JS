0a602e5099ec4e3ec665e15286f9a78dc43c0495
Smerlyn García <smerlyngarcia@presidencia.gob.do>
2022-09-20 11:06:14 -0400

    #108 - Trabajando con la creación de todos y creando una clase general para importar y exportar todas las clases desde ahí

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import { Todo } from './classes/todo.class';
+import { Todo, TodoList } from './classes/index.class';
 import './styles.css';
 
-const tarea = new Todo('Cazar patos');
-console.log(tarea); 
+
+const todoList = new TodoList();
+
+const tarea1 = new Todo('Cazar patos');
+const tarea2 = new Todo('Cultivar lechugas');
+
+todoList.nuevoTodo(tarea1);
+todoList.nuevoTodo(tarea2);
+
+console.log(todoList); 
