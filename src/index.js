1673aeea045deb8b4dd3abd501ecb451d4d3fe5c
Smerlyn García <smerlyngarcia@presidencia.gob.do>
2022-10-03 11:15:08 -0400

    #116 - Reconstruyendo instancias de Todos

--- src/index.js
+++ src/index.js
@@ -8,3 +8,6 @@ export const todoList = new TodoList();
 // console.log(todoList.todos);
 
 todoList.todos.forEach(crearTodoHtml)
+todoList.todos[0].imprimirClase();
+
+console.log('Todos', todoList.todos); 
