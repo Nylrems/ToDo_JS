a2f748e72b35670f88181b3e9cc8388bdd1e990a
Smerlyn García <smerlyngarcia@presidencia.gob.do>
2022-10-03 12:01:20 -0400

    #117 - Aplicando filtros a la seccion de pendientes, completados y todos

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -3,7 +3,9 @@ import { todoList } from '../index'
 //Referencias en el documento de HTML
 const divTodoList = document.querySelector('.todo-list');
 const txtInput = document.querySelector('.new-todo');
-const btnBorrarAllTodos = document.querySelector('.clear-completed')
+const btnBorrarAllTodos = document.querySelector('.clear-completed');
+const ulFiltros = document.querySelector('.filters');
+const anchorFiltros = document.querySelectorAll('.filtro')
 
 export const crearTodoHtml = (todo) => {
     const htmlTodo =
@@ -60,6 +62,35 @@ btnBorrarAllTodos.addEventListener('click', () => {
 
         if (elemento.classList.contains('completed')) {
             divTodoList.removeChild(elemento);
+        }
+    }
+});
+
+ulFiltros.addEventListener('click', (event) => {
+    // console.log(event.target.text); 
+
+    const filtro = event.target.text;
+    if (!filtro) { return; }
+
+    anchorFiltros.forEach(elem => elem.classList.remove('selected'));
+    event.target.classList.add('selected');
+
+    for (const elemento of divTodoList.children) {
+        elemento.classList.remove('hidden');
+        const completado = elemento.classList.contains('completed');
+
+        switch (filtro) {
+            case 'Pendientes':
+                if (completado) {
+                    elemento.classList.add('hidden');
+                }
+                break;
+
+            case 'Completados':
+                if (!completado) {
+                    elemento.classList.add('hidden');
+                }
+                break;
         }
     }
 });
